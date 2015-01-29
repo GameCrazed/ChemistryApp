a0e7211deb4ac89aa7ab81238a4d9f7997adae12
taziz3 <tyba967@gmail.com>
2015-01-29 22:47:56 +0000

    Dialog
    Added a button, dialog box appears, webview (HTML) is commented out for
    this in MainActivity and acitivty_main.xml.  Still testing this out.

--- PT/src/com/example/pt/MainActivity.java
+++ PT/src/com/example/pt/MainActivity.java
@@ -5,111 +5,131 @@ import java.io.InputStream;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-import android.R;
 
 public class MainActivity extends Activity {
 
-	@SuppressLint("SetJavaScriptEnabled")
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+        Button mainBtn;
 
-		// Create a new WebView for HTML to run
-		WebView webview = new WebView(this);
+                mainBtn = (Button) findViewById(R.id.button);
+        mainBtn.setOnClickListener(new OnClickListener() {
 
-		// Enable JavaScriptin WebView
-		webview.getSettings().setJavaScriptEnabled(true);
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                openAlert(v);
+            }
+        });
 
-		// Add the WebAppInterface class to the JavaScript so the HTML button
-		// can access the methods from the WebAppInterface class.
-		// JavaScriptInterface is called "ID".
-		webview.addJavascriptInterface(new WebAppInterface(this), "ID");
+//		// Create a new WebView for HTML to run
+//		WebView webview = new WebView(this);
+//
+//		// Enable JavaScriptin WebView
+//		webview.getSettings().setJavaScriptEnabled(true);
+//
+//		// Add the WebAppInterface class to the JavaScript so the HTML button
+//		// can access the methods from the WebAppInterface class.
+//		// JavaScriptInterface is called "ID".
+//		webview.addJavascriptInterface(new WebAppInterface(this), "ID");
+//
+//		setContentView(webview);
+//
+//		// Read & display elements from a HTML file in the WebView
+//		try {
+//			InputStream stream = this.getAssets().open("elements.html");
+//			int streamSize = stream.available();
+//			byte[] buffer = new byte[streamSize];
+//			stream.read(buffer);
+//			stream.close();
+//			String html = new String(buffer);
+//			webview.loadData(html, "text/html", "UTF-8");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 
-		setContentView(webview);
+    }
 
-		// Read & display elements from a HTML file in the WebView
-		try {
-			InputStream stream = this.getAssets().open("elements.html");
-			int streamSize = stream.available();
-			byte[] buffer = new byte[streamSize];
-			stream.read(buffer);
-			stream.close();
-			String html = new String(buffer);
-			webview.loadData(html, "text/html", "UTF-8");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public class WebAppInterface {
+        Context mContext;
 
-	}
+        /** Instantiate the interface and set the context */
 
-	public class image {
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
 
-		 public void change() {
-		
-		 ImageView image = (ImageView) (findViewById(R.id.imageView1));
-		 image.setImageResource(R.raw.a1);
-		 System.out.println("Image should have changed");
-		
-		 }
-	}
+        public void getID(String toast) {
+            // Show toast of the atomic symbol - ID
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
 
+            // Switch to another screen - start second activity
 
-	public class WebAppInterface {
-		Context mContext;
+            // ImageView image = (ImageView) (findViewById(R.id.imageView1));
+            // image.setImageResource(R.raw.a1);
+            Intent intent = new Intent(MainActivity.this, Element.class);
+            // startActivity(intent.putExtra("image", R.raw.a1));
 
-		/** Instantiate the interface and set the context */
-		WebAppInterface(Context c) {
-			mContext = c;
-		}
+            startActivity(intent);
 
-
-//		@JavascriptInterface
-		public void getID(String toast) {
-
-			// Switch to another screen - start second activity
-
-//			Intent intent = new Intent(MainActivity.this, Element.class);
-
-//			 startActivity(intent.putExtra("image", raw.a1));
-
-//			 startActivity(intent);
-// Show toast of the atomic symbol - ID
-//            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
-
-			// Using the ID, show a specific image on the second screen.
-
-            Intent myIntent = new Intent(MainActivity.this, Element.class);
-            startActivity(myIntent);
+            // Using the ID, show a specific image on the second screen.
 
 
-			}
-	}
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		 getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		 int id = item.getItemId();
-		 if (id == R.id.action_settings) {
-		 return true;
-		 }
-		return super.onOptionsItemSelected(item);
-	}
+    public void openAlert(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+
+        alertDialogBuilder.setTitle(this.getTitle()+ " decision");
+        alertDialogBuilder.setMessage("Are you sure?");
+
+        // set neutral button: Exit the app message
+        alertDialogBuilder.setNeutralButton("Exit the app",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int id) {
+                // exit the app and go to the HOME
+                MainActivity.this.finish();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show alert
+        alertDialog.show();
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
